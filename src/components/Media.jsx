34d9bbf2a4d8fee38c88aa8d9d8ec052310eac7d
Diego <diego.noblecilla@wayni.pe>
2023-09-08 10:10:10 -0500

    2do shower

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -9,7 +9,7 @@ export const Media = ({ url, width, height, name, mute, loop = true, play = true
                 <ReactPlayer className='react-player' width={width} height={height} url={url} muted={mute} playing={play} loop={loop} />
             </div>
             {mute ?
-                (<a className='ref' href='https://goo.gl/maps/Nxsh3DVCJC2USRPt9' target='_blank'></a>)
+                (<a className='ref' href='https://goo.gl/maps/U5Mi9UHZ9VS7zd2w5' target='_blank'></a>)
                 :
                 null
             }
